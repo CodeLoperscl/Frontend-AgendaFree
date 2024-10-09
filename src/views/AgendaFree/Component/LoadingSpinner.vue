70ea93eb008e65f3b9676d621d09bc2f41f2f5e6
kelsier01 <b.fuentes38@alumnos.santotomas.cl>
2024-10-09 17:23:55 -0300

    ultimos cambios

--- src/views/AgendaFree/Component/LoadingSpinner.vue
+++ src/views/AgendaFree/Component/LoadingSpinner.vue
@@ -1,40 +1,76 @@
 <template>
-    <div v-if="isLoading" class="overlay">
-      <div class="text-center">
-        <div class="spinner-border text-light" role="status">
-          <span class="visually-hidden">Cargando...</span>
-        </div>
-        <p class="text-light mt-3">Cargando...</p>
-      </div>
+  <div v-if="isLoading" class="spinner-overlay">
+    <div class="loader">
+      <div class="orbe" style="--index: 0"></div>
+      <div class="orbe" style="--index: 1"></div>
+      <div class="orbe" style="--index: 2"></div>
+      <div class="orbe" style="--index: 3"></div>
+      <div class="orbe" style="--index: 4"></div>
     </div>
-  </template>
-  
-  <script setup>
-  import { defineProps } from 'vue';
-  
-  // Definimos el prop que recibe el estado de carga
-  const props = defineProps({
-    isLoading: {
-      type: Boolean,
-      required: true,
-    },
-  });
-  </script>
-  
-  <style scoped>
-  /* Estilos para el fondo opaco */
-  .overlay {
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-color: rgba(0, 0, 0, 0.5); /* Fondo oscuro con opacidad */
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    z-index: 9999;
-  }
-  </style>
+  </div>
+</template>
+
+
+<script setup>
+
+import { defineProps } from 'vue';
+
+// Definimos el prop que recibe el estado de carga
+const props = defineProps({
+  isLoading: {
+    type: Boolean,
+    required: true,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+
+$blanco-marfil: #fafafa;
+$verde-azulado: #16a085;
+$verde-pastel: #d1f2eb;
+/* Estilos para el fondo opaco */
+.spinner-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba($blanco-marfil, 0.8);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 9999;
+}
+
+.orbe {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  --delay: calc(var(--index) * 0.1s);
+  animation: orbit7456 ease-in-out 1.5s var(--delay) infinite;
+  opacity: calc(1 - calc(0.2 * var(--index)));
+}
+
+.orbe::after {
+  position: absolute;
+  content: "";
+  top: 0;
+  left: 0;
+  width: var(--size-orbe);
+  height: var(--size-orbe);
+  background-color: $verde-azulado;
+  box-shadow: 0px 0px 20px 2px $verde-azulado;
+  border-radius: 50%;
+}
+.loader {
+  --size-loader: 50px;
+  --size-orbe: 10px;
+  width: var(--size-loader);
+  height: var(--size-loader);
+  position: relative;
+  transform: rotate(45deg);
+}
+</style>
   
   
--- src/views/AgendaFree/LandingEspecialista/vistas/LandingEspecialistaView.vue
+++ src/views/AgendaFree/LandingEspecialista/vistas/LandingEspecialistaView.vue
@@ -198,15 +198,6 @@ onMounted(() => {
 
 <template>
   <LoadingSpinner :isLoading="isLoading" />
-  <div v-if="isLoading" class="spinner-overlay">
-    <div class="loader">
-      <div class="orbe" style="--index: 0"></div>
-      <div class="orbe" style="--index: 1"></div>
-      <div class="orbe" style="--index: 2"></div>
-      <div class="orbe" style="--index: 3"></div>
-      <div class="orbe" style="--index: 4"></div>
-    </div>
-  </div>
   <div class="hero d-flex align-items-center justify-content-center">
     <div class="animated-background">
       <div class="shape shape-1"></div>
@@ -431,48 +422,6 @@ h2 {
   }
 }
 
-.spinner-overlay {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba($blanco-marfil, 0.8);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 9999;
-}
-
-.loader {
-  --size-loader: 50px;
-  --size-orbe: 10px;
-  width: var(--size-loader);
-  height: var(--size-loader);
-  position: relative;
-  transform: rotate(45deg);
-}
-
-.orbe {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  --delay: calc(var(--index) * 0.1s);
-  animation: orbit7456 ease-in-out 1.5s var(--delay) infinite;
-  opacity: calc(1 - calc(0.2 * var(--index)));
-}
-
-.orbe::after {
-  position: absolute;
-  content: "";
-  top: 0;
-  left: 0;
-  width: var(--size-orbe);
-  height: var(--size-orbe);
-  background-color: $verde-azulado;
-  box-shadow: 0px 0px 20px 2px $verde-azulado;
-  border-radius: 50%;
-}
 
 @keyframes orbit7456 {
   0% {
